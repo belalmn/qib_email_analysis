3c56950a37fce2bb94e69690c524f81779c39d11
Belal Mohammed-Nur <belal.mnur@gmail.com>
2024-08-17 23:09:48 +0300

    Added Hugging Face download tool and updated llm inference.

--- src/transform/llm_tools.py
+++ src/transform/llm_tools.py
@@ -1,14 +1,36 @@
-from langchain_community.llms.ollama import Ollama
+# from langchain_community.llms.ollama import Ollama
 
-llms = {}
+# llms = {}
 
 
-def invoke_llm(text: str, model: str = "llama2:7B") -> str:
-    if model not in llms:
-        try:
-            llms[model] = Ollama(model=model)
-        except Exception as e:
-            print(e)
+# def invoke_llm(text: str, model: str = "llama2:7B") -> str:
+#     if model not in llms:
+#         try:
+#             llms[model] = Ollama(model=model)
+#         except Exception as e:
+#             print(e)
 
-    llm = llms[model]
-    return llm.invoke(text)
+#     llm = llms[model]
+#     return llm.invoke(text)
+
+import torch
+import transformers
+from langchain import HuggingFacePipeline, LLMChain, PromptTemplate
+from transformers import AutoTokenizer
+
+model = "meta-llama/Llama-2-7b-chat-hf"
+tokenizer = AutoTokenizer.from_pretrained(model)
+
+pipeline = transformers.pipeline(
+    task="text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    device="cuda" if torch.cuda.is_available() else "cpu",
+    max_length=3000,
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    do_sample=True,
+)
+
+def invoke_llm(text: str) -> str:
+    return pipeline(text)[0]["generated_text"]
